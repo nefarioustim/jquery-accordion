82538f4df16493c119aa1516da36c9afde19e057
Tim Huegdon <tim@nefariousdesigns.co.uk>
2010-08-21 22:19:25 +0100

    All functions added.

--- js/jquery.accordion.2.0.js
+++ js/jquery.accordion.2.0.js
@@ -11,8 +11,10 @@
     
     $.fn.accordion = function(config) {
         var defaults = {
-            "handle": "h3",
-            "panel": ".panel"
+            "handle":       "h3",
+            "panel":        ".panel",
+            "accordion":    true,
+            "toggle":       false
         };
         
         if (config) $.extend(defaults, config);
@@ -50,6 +52,7 @@
                                                     paddingBottom: panel.data("dimensions").paddingBottom
                                                 }, {
                                                     duration:   "normal",
+                                                    queue:      false,
                                                     complete:   function(){
                                                         panel.addClass("open");
                                                         clickedLi.addClass("active");
@@ -70,6 +73,7 @@
                                                     paddingBottom: 0
                                                 }, {
                                                     duration:   "normal",
+                                                    queue:      false, 
                                                     complete:   function(){
                                                         panel.removeClass("open").hide();
                                                         clickedLi.removeClass("active");
@@ -83,18 +87,24 @@
                 handles     = accordion.find(">li>" + defaults.handle)
                                 .wrapInner('<a class="opener" href="#open-panel" />');
             
+            accordion.find("> li.active > .panel, > li.expanded > .panel").show().addClass("open");
+            
             accordion.delegate(".opener", "click", function(e){
                 e.preventDefault();
                 e.stopImmediatePropagation();
                 
                 var clicked     = $(this),
                     clickedLi   = clicked.closest("li"),
-                    panel       = clickedLi.find(">" + defaults.panel).first();
+                    panel       = clickedLi.find(">" + defaults.panel).first(),
+                    open        = accordion.find(">li:not(.expanded)>" + defaults.panel + ".open");
                 
-                if (panel.is(":visible")) {
-                    panel.trigger("panel-close", [clickedLi]);
-                } else {
-                    panel.trigger("panel-open", [clickedLi]);
+                if (!clickedLi.hasClass("expanded")) {
+                    if (panel.is(":visible")) {
+                        if (defaults.toggle) panel.trigger("panel-close", [clickedLi]);
+                    } else {
+                        panel.trigger("panel-open", [clickedLi]);
+                        if (defaults.accordion) open.trigger("panel-close", [clickedLi]);
+                    }
                 }
             });
         });
